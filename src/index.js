69d37f33456e30cac50b26ae01519eb37fd80ec8
DanielEspinoza77 <espinozadani96@gmail.com>
2025-09-10 15:45:48 -0600

    my first model

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const sequelize = require('./config/db');
 const routes = require('./routes/index.js')
+const Recorrido = require('./models/usermodels.js')
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
